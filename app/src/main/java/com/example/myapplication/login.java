2effb872784b5da306bd41bf277238e2ad0a0660
Eze Evidence <eze.pleasant234@gmail.com>
2019-02-19 12:13:19 +0100

    New Commit

--- app/src/main/java/com/example/myapplication/login.java
+++ app/src/main/java/com/example/myapplication/login.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Button button = (Button) findViewById(R.id.register);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(login.this, MainActivity.class));
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/splashscreen.java
+++ app/src/main/java/com/example/myapplication/splashscreen.java
@@ -1,13 +1,27 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class splashscreen extends AppCompatActivity {
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+   mHandler.postDelayed(new Runnable() {
+       @Override
+       public void run() {
+           Intent intent = new Intent(splashscreen.this,login.class);
+           startActivity(intent);
+       }
+
+   }, 6000);
     }
 }
