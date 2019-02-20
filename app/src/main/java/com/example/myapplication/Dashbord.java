7116469633c000e8ed6fd80fdd69628111115565
Eze Evidence <eze.pleasant234@gmail.com>
2019-02-20 12:13:26 +0100

    New commita

--- app/src/main/java/com/example/myapplication/Dashbord.java
+++ app/src/main/java/com/example/myapplication/Dashbord.java
@@ -1,19 +1,13 @@
 package com.example.myapplication;
 
-import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Splashscreen extends AppCompatActivity {
-
+public class Dashbord extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splashscreen);
-
-
-        }
+        setContentView(R.layout.activity_dashbord);
     }
-
+}
